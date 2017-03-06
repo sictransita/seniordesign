06f1d8b115c40d02c6d69c4d2ec6b786451d21d0
Philip Johnston <Philip@Philips-MacBook-Pro.local>
2017-03-06 17:36:37 -0600

    updated c simulation and uploading simulation executable

--- mc_sim.c
+++ mc_sim.c
@@ -1,50 +1,74 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
+#include <time.h>
 
-#define ITERATIONS 10000;
-#define ALPHA 2.3;
-#define BETA 3.1;
-#define MU 1.2;
-#define DT 0.001;
-#define SIGMA_0 = 3.4;
-#define S_0 = 100.0;
+#ifndef max
+#define max(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a > _b ? _a : _b; })
+#endif
+#define ITERATIONS 252 // days in stock market per year
+#define ALPHA 0.03
+#define BETA 0.013
+#define MU 0.2
+#define DT 1
+#define R 0.02
 
-// variable arrays
-float sigma[] = new float[ITERATIONS];
-float q[] = new float[ITERATIONS];
-float w[] = new float[ITERATIONS];
-float s[] = new float[ITERATIONS];
+double sigma[ITERATIONS] = {1.2};
+double s[ITERATIONS] = {100.0};
+double q[ITERATIONS] = {0.0};
+double w[ITERATIONS] = {0.0};
 
-int main()
-{
-  float lambda;
-  float epsilon;
-  float sigma_sq;
-  float prev_sigma_sq;
-
-  // set initial sigma in array
-  sigma[0] = SIGMA_0;
-  s[0] = S_0;
-  q[0] = 0.0;
-  w[0] = 0.0;
-  prev_sigma_sq = SIGMA_0 * SIGMA_0;
-
-  // calculate core inputs
+static double stock_price() {
   for (int i = 1; i < ITERATIONS; i++) {
-    lambda = math.random();
-    epsilon = math.random();
-
-    // sigma calculations
-    sigma_sq = sigma[0] + (ALPHA * prev_sigma_sq) + (BETA * prev_sigma_sq * lambda * lambda);
-    prev_sigma_sq = sigma_sq;
-    sigma[i] = math.sqrt(sigma_sq);
-
-    // core input calculations
-    q[i] = (1 + MU * DT) - sigma_sq * DT * 0.5;
-    w[i] = math.sqrt(DT) * sigma[i];
-
-    // stock price calculations
-    s[i] = q[i] + epsilon * w[i];
+    double lambda = (double)rand()/(double)(RAND_MAX);
+    double epsilon = (double)rand()/(double)(RAND_MAX);
+    double temp = sigma[0] + ALPHA * sigma[i-1] * sigma[i-1] + BETA * sigma[i-1] * sigma[i-1] * lambda * lambda;
+    sigma[i] = sqrt(temp);
+    q[i] = 1 + MU*DT - temp * DT * 0.5;
+    w[i] = sigma[i] * sqrt(DT);
+    s[i] = s[i-1] * (q[i] + epsilon * w[i]);
+    // printf("previous stock is %f\n", s[i-1]);
+    // printf("stock is %f\n", s[i]);
   }
+  return s[ITERATIONS - 1];
+}
+
+static double payoff(double s_final, double k) {
+  double payoff = max(0.0, s_final - k);
+  return payoff * exp(-R);
+}
+
+int main () {
+  // begin timing
+  clock_t begin = clock();
+
+  // sigma[0] = 1.2;
+  // s[0] = 100.0;
+  // q[0] = 0.0;
+  // w[0] = 0.0;
+
+  double sum_payoff = 0.0;
+  double avg_payoff = 0.0;
+  double temp = 0.0;
+  double stock = 0.0;
+
+  // find the stock price with payoff and sum up
+  for (int i = 0; i < 30; i++) {
+    stock = stock_price();
+    temp = payoff(stock, 110.0);
+    sum_payoff = sum_payoff + temp;
+  }
+  avg_payoff = sum_payoff / 30.0;
+
+  // end timing
+  clock_t end = clock();
+  double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+
+  printf("The average payoff is %f\n", avg_payoff);
+  printf("The program took %f seconds to execute", time_spent);
+
   return 0;
 }
