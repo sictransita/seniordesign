68fea2a959fb0a692437741bbef74ed634772661
sictransita <johnstonpt@email.wustl.edu>
2017-03-22 19:48:14 -0500

    d

--- sqrt_conv.vhd
+++ sqrt_conv.vhd
@@ -5,6 +5,9 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+library ieee_proposed;
+use ieee_proposed.fixed_pkg.all;
+
 entity square_root is
 	generic(
 		WIDTH	: positive := 32
