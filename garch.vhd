3271e281d19bfb62707881f87e7eb0ca5781e0e4
sictransita <johnstonpt@email.wustl.edu>
2017-04-10 11:20:44 -0500

    8 bit updated

--- garch.vhd
+++ garch.vhd
@@ -8,28 +8,42 @@ use ieee_proposed.fixed_pkg.all;
 
 entity garch is
   port  ( clk : in std_logic;
-          lambda : in ufixed (0 downto -31);
-          epsilon : in ufixed (0 downto -31);
-          alpha : in ufixed (0 downto -31);
-          beta : in ufixed (0 downto -31);
-          sigma0 : in ufixed (0 downto -31);
+          lambda : in ufixed (0 downto -7);
+          epsilon : in ufixed (0 downto -7);
+          alpha : in ufixed (0 downto -7); -- taken out
+          beta : in ufixed (0 downto -7); -- beta taken out
+          sigma0 : in ufixed (0 downto -7); -- taken out
 
-          q : out ufixed (0 downto -31);
-          weps : out ufixed (0 downto -31);
-          sigma : out ufixed (0 downto -31));
+          q : out ufixed (0 downto -7);
+          weps : out ufixed (0 downto -7);
+          sigma : out ufixed (0 downto -7)); -- necessary?
 end garch;
 
 architecture pipelined of garch is
-  -- iternal signals from flop inputs/ to flop outputs
-  signal in_lambda : ufixed (0 downto -31) := "00000000000000000000000000000000";
-  signal in_epsilon : ufixed (0 downto -31) := "00000000000000000000000000000000";
-  signal in_alpha : ufixed (0 downto -31) := "00000000000000000000000000000000";
-  signal in_beta : ufixed (0 downto -31) := "00000000000000000000000000000000";
-  signal in_sigma0 : ufixed (0 downto -31) := "00000000000000000000000000000000";
 
-  signal out_q : ufixed (0 downto -31) := "00000000000000000000000000000000";
-  signal out_weps : ufixed (0 downto -31) := "00000000000000000000000000000000";
-  signal out_sigma : ufixed (0 downto -31) := "00000000000000000000000000000000";
+  -- sqrt component
+  component sqrt_pipelined
+  port ( clk : in std_logic;
+			x : in unsigned (7 downto 0);
+			osqrt : out unsigned (7 downto 0);
+			odebug : out unsigned (7 downto 0));
+  end component;
+		
+  -- iternal signals from flop inputs/ to flop outputs
+  signal in_lambda : ufixed (0 downto -7) := "00000000";
+  signal in_epsilon : ufixed (0 downto -7) := "00000000";
+  signal in_alpha : ufixed (0 downto -7) := "00000000"; -- decimal 0.155900
+  signal in_beta : ufixed (0 downto -7) := "00000000"; -- 0.840000
+  signal in_sigma0 : ufixed (0 downto -7) := "00000000";
+  signal in_sqrt : unsigned (7 downto 0) := "00000000";
+  
+  signal sqrt : unsigned (7 downto 0) := "00000000";
+  signal sqrt_rem : unsigned (7 downto 0) := "00000000";
+  signal out_sqrt : ufixed (0 downto -7) := "00000000";
+  signal out_sqrt_rem : ufixed (0 downto -7) := "00000000";
+  signal out_q : ufixed (0 downto -7) := "00000000";
+  signal out_weps : ufixed (0 downto -7) := "00000000";
+  signal out_sigma : ufixed (0 downto -7) := "00000000";
 
 
   -- wires in garch pipeline (refer to block diagram on google drive)
@@ -37,36 +51,27 @@ architecture pipelined of garch is
             --> ex. w1to2 means wire from stage 1 to 2
   --   letter prefix (r, l, or c) denotes right, left, center respectively
             --> ex r3 means wire on the right in stage 3
-  signal w1to2 : ufixed (0 downto -31) := "00000000000000000000000000000000";
-  signal w2to3 : ufixed (0 downto -31) := "00000000000000000000000000000000";
-  signal l3 : ufixed (0 downto -31) := "00000000000000000000000000000000";
-  signal r3 :ufixed (0 downto -31) := "00000000000000000000000000000000";
-  signal w3to4 : ufixed (0 downto -31) := "00000000000000000000000000000000";
-  signal l4to5 : ufixed (0 downto -31) := "00000000000000000000000000000000";
-  signal c4to5 : ufixed (0 downto -31) := "00000000000000000000000000000000";
-  signal r4to5 : ufixed (0 downto -31) := "00000000000000000000000000000000";
+  signal w1to2 : ufixed (0 downto -7) := "00000000";
+  signal w2to3 : ufixed (0 downto -7) := "00000000";
+  signal l3 : ufixed (0 downto -7) := "00000000";
+  signal r3 :ufixed (0 downto -7) := "00000000";
+  signal w3to4 : ufixed (0 downto -7) := "00000000";
+  signal l4to5 : ufixed (0 downto -7) := "00000000";
+  signal c4to5 : ufixed (0 downto -7) := "00000000";
+  signal r4to5 : ufixed (0 downto -7) := "00000000";
 
   -- constants used in pipeline
-  signal gamma : ufixed (0 downto -31) := "00000000000000000000000000000000"; -- dt/2
-  signal eta : ufixed (0 downto -31) := "00000000000000000000000000000000"; -- 1 + mu*dt
-  signal theta : ufixed (0 downto -31) := "00000000000000000000000000000000"; -- sqrt(dt)
+  signal gamma : ufixed (0 downto -7) := "00000000"; -- dt/2
+  signal eta : ufixed (0 downto -7) := "00000000"; -- 1 + mu*dt
+  signal theta : ufixed (0 downto -7) := "00000000"; -- sqrt(dt)
 
-  -- components (square root unit)
-  --component square_root
-  --generic (WIDTH : positive := 32);
-  --port (  clk	: in std_logic;
-  --        res	: in std_logic;
-  --        ARG	: in unsigned (WIDTH - 1 downto 0);
-  --        Z	: out unsigned (WIDTH - 1 downto 0));
-  --end component;
   
   begin
-  -- map ports
-  --  root: square_root
-  --    port map  ( clk => clk,
-  --                res => '0',
-  --                ARG => to_unsigned(w3to4, 32),
-  --                z => root_out);
+    root: sqrt_pipelined
+      port map  ( clk => clk,
+                  x => in_sqrt,
+                  osqrt => sqrt,
+						odebug => sqrt_rem);
 						
   -- process for pipeline
   process(clk)
@@ -84,30 +89,35 @@ architecture pipelined of garch is
       q <= out_q;
       weps <= out_weps;
       sigma <= out_sigma;
-
+		
+		-- root i/o conversions
+		in_sqrt <= to_unsigned(w3to4, 8);
+		out_sqrt <= to_ufixed(sqrt);
+		out_sqrt_rem <= to_ufixed(sqrt_rem);
+		
       -- pipeline loop
       for i in 1 to 7 loop
         case i is
           -- stage 1
-          when 1 => w1to2 <= in_lambda(0 downto -15) * in_lambda(0 downto -15);
+          when 1 => w1to2 <= in_lambda(0 downto -3) * in_lambda(0 downto -3);
 
           -- stage 2
-          when 2 => w2to3 <= w1to2(0 downto -15) * in_beta(0 downto -15);
+          when 2 => w2to3 <= w1to2(0 downto -3) * in_beta(0 downto -3);
 
           -- stage 3
-          when 3 => l3 <= w3to4(0 downto -15) * w2to3(0 downto -15);
-                    r3 <= w3to4(0 downto -15) * in_alpha(0 downto -15);
-                    w3to4 <= resize(l3 + r3 + in_sigma0, 0, -31);
+          when 3 => l3 <= w3to4(0 downto -3) * w2to3(0 downto -3);
+                    r3 <= w3to4(0 downto -3) * in_alpha(0 downto -3);
+                    w3to4 <= resize(l3 + r3 + in_sigma0, 0, -7);
 
           -- stage 4
-          when 4 => c4to5 <= w3to4;
-                    r4to5 <= in_epsilon(0 downto -15) * theta(0 downto -15);
-                    l4to5 <= w3to4(0 downto -15) * gamma(0 downto -15);
-                    out_sigma <= w3to4;
+          when 4 => c4to5 <= out_sqrt;
+                    r4to5 <= in_epsilon(0 downto -3) * theta(0 downto -3);
+                    l4to5 <= w3to4(0 downto -3) * gamma(0 downto -3);
+                    out_sigma <= out_sqrt;
 
           -- stage 5
-          when 5 => out_weps <= r4to5(0 downto -15) * c4to5(0 downto -15);
-                    out_q <= resize(eta - l4to5, 0, -31);
+          when 5 => out_weps <= r4to5(0 downto -3) * c4to5(0 downto -3);
+                    out_q <= resize(eta - l4to5, 0, -7);
 
           when others => null;
         end case;
--- pipelined_fixed_point_elementary_functions/trunk/math_pipelined/sqrt_pipelined.v
+++ pipelined_fixed_point_elementary_functions/trunk/math_pipelined/sqrt_pipelined.v
@@ -1,7 +1,3 @@
-
-
-
-
 module sqrt_pipelined(
 	input clk,
 	input [UP:0] x,
@@ -9,19 +5,16 @@ module sqrt_pipelined(
 	output [UP:0] odebug
 	);
 
-parameter BITS= 32;
-//	����������� ���������, �� ��������������.
+parameter BITS= 8;
 
-//	����������� ���������
 parameter UP= BITS-1;
 
 reg [UP:0] cr [UP:0];
 reg [UP:0] crr [UP:0];
-reg [UP:0] _crr [UP:0];
 
 reg [UP:0] cx [UP:0];
 
-`define BOUT (UP)	//	����� ���������� ������������� ������ ��� �������
+`define BOUT (UP)	
 
 assign osqrt= cr[`BOUT];
 assign odebug= crr[`BOUT];
@@ -30,11 +23,7 @@ always @( posedge clk )
 begin:name
 	integer ind;
 
-
-	//	�������� �������� � ������������ ����
 	cx[0]<= x;
-//	cr[0]<=  32'h00800000;
-//	crr[0]<= 32'h40000000;
 `define MEDI  (64'h0000000000000001<<(BITS-1) )
 `define MEDI2 (64'h0000000000000001<<(BITS-1))
 	cr[0]<=  `MEDI;
@@ -46,13 +35,11 @@ begin:name
 		if ( cx[ind]>crr[ind] )
 		begin
 			cr [ind+1]<= cr [ind] + (`MEDI>>(ind+1));
-//			_crr [ind+1]<= (cr [ind] + (`MEDI>>(ind+1)) )*(cr [ind] + (`MEDI>>(ind+1)) );
 			crr[ind+1]<= crr [ind] + (`MEDI2>>(2*(ind+1))) + ( (cr[ind])>>(ind) );
 		end
 		else
 		begin
 			cr [ind+1]<= cr [ind] - (`MEDI>>(ind+1));
-//			_crr [ind+1]<= (cr [ind] - (`MEDI>>(ind+1)) )*(cr [ind] - (`MEDI>>(ind+1)) );
 			crr[ind+1]<= crr [ind] + (`MEDI2>>(2*(ind+1))) - ( (cr[ind])>>(ind) );
 		end
 		cx [ind+1]<= cx [ind];
